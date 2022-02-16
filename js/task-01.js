0bdc5dec8a21cfe8b58f1bad0fb7abdb7657a273
Vadim Bogomolov <vadim.bogomolov@gmail.com>
2022-02-16 20:38:10 +0100

    task-01 complete

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoriesEl = document.querySelectorAll('.item');
+console.log('Number of categories ', categoriesEl.length);
+
+categoriesEl.forEach(category => {
+    console.log('Category: ', category.firstElementChild.textContent);
+    console.log('Elements: ', category.lastElementChild.children.length);
+});
